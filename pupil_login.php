2ba76135f423692e53851c07d320fac3c85b5e25
saladbut99 <lerin113@gmail.com>
2021-10-02 21:24:53 +0800

    new

---
+++ pupil_login.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title></title>
+    <link rel="stylesheet" href="mystyle.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+    <title></title>
+  </head>
+  <body>
+    <center>
+    <div class="formcontainer" style="max-width:70%;">
+      <div class="container h-100">
+        <div class="row h-100 align-items-center">
+              <h1><b>Pupil Login</b></h1>
+                <div class="col-md text-center">
+                      <h1 style="font-size:20px;"><b>Pupil</b></h1>
+                </div>
+                <div class="col-md text-center">
+                      <h1 style="font-size:20px;"><b>Teacher</b></h1>
+                </div>
+                <div class="col-md text-center">
+                      <h1 style="font-size:20px;"><b>Admin</b></h1>
+                </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </center>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js" integrity="sha384-W8fXfP3gkOKtndU4JGtKDvXbO53Wy8SZCQHczT5FMiiqmQfUpWbYdTil/SxwZgAN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.min.js" integrity="sha384-skAcpIdS7UcVUC05LJ9Dxay8AXcDYfBJqt1CJ85S/CFujBsIzCIv+l9liuYLaMQ/" crossorigin="anonymous"></script>
+  </body>
+</html>
