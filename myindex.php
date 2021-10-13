6f743f5cd69430f4210bc87e9358f7ed7324f297
Migz <apaga.miggy_vincent@hnu.edu.ph>
2021-10-13 22:33:46 +0800

    responsive animation

--- myindex.php
+++ myindex.php
@@ -6,8 +6,14 @@
     <title>Pulong</title>
     <link rel="stylesheet" href="mystyle2.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
-
+    <script
+        src="https://code.jquery.com/jquery-3.6.0.min.js"
+        integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4="
+        crossorigin="anonymous">
+    </script>
+    <script src="jquery.js"></script>
   </head>
+
   <body id="body">
   <div class="bg" id="content_dark1">
     <div class="container h-100">
@@ -26,7 +32,7 @@
   </div>
 
   <br><br>
-
+<div class="snap"></div>
 <div class="whitebg" id="content_dark2">
   <div class="container h-50">
     <div class="row h-100 align-items-center">
@@ -223,5 +229,6 @@
 
   <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js" integrity="sha384-W8fXfP3gkOKtndU4JGtKDvXbO53Wy8SZCQHczT5FMiiqmQfUpWbYdTil/SxwZgAN" crossorigin="anonymous"></script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.min.js" integrity="sha384-skAcpIdS7UcVUC05LJ9Dxay8AXcDYfBJqt1CJ85S/CFujBsIzCIv+l9liuYLaMQ/" crossorigin="anonymous"></script>
-  </body>
+  
+</body>
 </html>
